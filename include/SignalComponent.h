c0d079483e5923e9bd35b5ba4e7f11eae78a8320
Theodore DeRego <tderego94@gmail.com>
2013-12-03 13:36:32 -1000

    Untested signals beware

--- include/SignalComponent.h
+++ include/SignalComponent.h
@@ -1,6 +1,8 @@
 #ifndef SIGNALCOMPONENT_H
 #define SIGNALCOMPONENT_H
 
+#include <SFML/System/Vector2.hpp>
+
 #include <Fission/Core/Component.h>
 
 class SignalComponent : public Component
@@ -8,22 +10,42 @@ class SignalComponent : public Component
     public:
         enum SignalType
         {
+            None,
             Integer,
-            Real,
+            Float,
             Vector
         };
 
         SignalComponent();
         virtual ~SignalComponent();
 
+        void addOutput(SignalComponent* output){outputs.push_back(output);}
+
         void fireSignal(int i);
+        void fireSignal(float f);
+        void fireSignal(const sf::Vector2f& v);
+
+        bool hasSignal(){return type!=SignalComponent::None;}
+        int getInt();
+        float getFloat();
+        sf::Vector2f getVector();
 
         static TypeBits Type;
         const TypeBits getTypeBits() const {return Type;}
         static Component* factory() {return new SignalComponent;}
 
     private:
+        void clearData();
+
+        void receiveSignal(int i);
+        void receiveSignal(float f);
+        void receiveSignal(const sf::Vector2f& v);
+
         std::vector<SignalComponent*> outputs;
+
+        // Received data
+        SignalType type;
+        void* data;
 };
 
 #endif // SIGNALCOMPONENT_H
--- src/SignalComponent.cpp
+++ src/SignalComponent.cpp
@@ -2,7 +2,7 @@
 
 TypeBits SignalComponent::Type;
 
-SignalComponent::SignalComponent()
+SignalComponent::SignalComponent() : type(SignalComponent::None), data(NULL)
 {
     //ctor
 }
@@ -15,7 +15,93 @@ SignalComponent::~SignalComponent()
 void SignalComponent::fireSignal(int i)
 {
     for (auto output : outputs)
-    {
-
-    }
+        output->receiveSignal(i);
 }
+
+void SignalComponent::fireSignal(float f)
+{
+    for (auto output : outputs)
+        output->receiveSignal(f);
+}
+
+void SignalComponent::fireSignal(const sf::Vector2f& v)
+{
+    for (auto output : outputs)
+        output->receiveSignal(v);
+}
+
+void SignalComponent::clearData()
+{
+    switch (type)
+    {
+    case SignalComponent::Integer:
+        delete reinterpret_cast<int*>(data);
+        break;
+    case SignalComponent::Float:
+        delete reinterpret_cast<float*>(data);
+        break;
+    case SignalComponent::Vector:
+        delete reinterpret_cast<sf::Vector2f*>(data);
+        break;
+    }
+
+    type = SignalComponent::None;
+}
+
+void SignalComponent::receiveSignal(int i)
+{
+    clearData();
+    type = SignalComponent::Integer;
+    data = new int(i);
+}
+
+void SignalComponent::receiveSignal(float f)
+{
+    clearData();
+    type = SignalComponent::Float;
+    data = new float(f);
+}
+
+void SignalComponent::receiveSignal(const sf::Vector2f& v)
+{
+    clearData();
+    type = SignalComponent::Vector;
+    data = new sf::Vector2f(v);
+}
+
+int SignalComponent::getInt()
+{
+    if (type == SignalComponent::Integer)
+    {
+        void* oData = data;
+        clearData();
+        return *reinterpret_cast<int*>(oData);
+    }
+    else
+        return 0;
+}
+
+float SignalComponent::getFloat()
+{
+    if (type == SignalComponent::Integer)
+    {
+        void* oData = data;
+        clearData();
+        return *reinterpret_cast<float*>(oData);
+    }
+    else
+        return 0;
+}
+
+sf::Vector2f SignalComponent::getVector()
+{
+    if (type == SignalComponent::Integer)
+    {
+        void* oData = data;
+        clearData();
+        return *reinterpret_cast<sf::Vector2f*>(oData);
+    }
+    else
+        return sf::Vector2f(0, 0);
+}
+
